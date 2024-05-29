ba850c086b8ed8ab38b6b34f61fac853f4ed7f76
weiz-98 <qweqwe928928@gmail.com>
2024-05-29 23:36:09 +0800

    feat: use json to encode data

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
-// Declare a handler which writes a plain-text response with information about the
-// application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Create a map which holds the information that we want to send in the response.
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	// Pass the map to the json.Marshal() function. This returns a []byte slice
+	// containing the encoded JSON. If there was an error, we log it and send the client
+	// a generic error message.
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+	// Append a newline to the JSON. This is just a small nicety to make it easier to
+	// view in terminal applications.
+	js = append(js, '\n')
+	// At this point we know that encoding the data worked without any problems, so we
+	// can safely set any necessary HTTP headers for a successful response.
+	w.Header().Set("Content-Type", "application/json")
+	// Use w.Write() to send the []byte slice containing the JSON as the response body.
+	w.Write(js)
 }
