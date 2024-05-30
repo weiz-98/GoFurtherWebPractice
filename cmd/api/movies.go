2b2cff8a471faa76649af81f92d8de0a7ec8f5ee
weiz-98 <qweqwe928928@gmail.com>
2024-05-31 03:05:17 +0800

    feat: add JSON decoding in createMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"GoFurtherWebPractice/internal/data" // New import
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
-// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
-// return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	// Declare an anonymous struct to hold the information that we expect to be in the
+	// HTTP request body (note that the field names and types in the struct are a subset
+	// of the Movie struct that we created earlier).
+	// This struct will be our *target decode destination*
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+	// Initialize a new json.Decoder instance which reads from the request body, and
+	// then use the Decode() method to decode the body contents into the input struct.
+	// when we call Decode() we pass a *pointer* to the input struct as the target decode destination.
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	// Dump the contents of the input struct in a HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,3 +57,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+// 使用 json.Unmarshal() 它並沒有比 json.Decoder 來得好。
+// 程式碼不僅稍微冗長，效率也較低。
