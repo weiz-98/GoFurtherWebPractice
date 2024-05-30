6b718d5058a7bb4e2986101d48a84ebd00bd2be8
weiz-98 <qweqwe928928@gmail.com>
2024-05-30 23:47:20 +0800

    feat: add errors.go to handle sending error messages

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -16,7 +16,8 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		// Use the new notFoundResponse() helper.
+		app.notFoundResponse(w, r)
 		return
 	}
 	// Create a new instance of the Movie struct, containing the ID we extracted from
@@ -34,7 +35,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	// Create an envelope{"movie": movie} instance and pass it to writeJSON(), instead // of passing the plain movie struct.
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
 	}
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,13 +8,14 @@ import (
 
 func (app *application) routes() *httprouter.Router { // Initialize a new httprouter router instance.
 	router := httprouter.New()
-	// Register the relevant methods, URL patterns and handler functions for our
-	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
-	// http.MethodPost are constants which equate to the strings "GET" and "POST"
-	// respectively.
+	// Convert the notFoundResponse() helper and methodNotAllowedResponse()
+	// helper to a http.Handler using the http.HandlerFunc() adapter
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
-	// Return the httprouter instance.
+
 	return router
 }
