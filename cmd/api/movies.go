5699b9e2c51457ec7cb54ab5b6236813f0932651
weiz-98 <qweqwe928928@gmail.com>
2024-06-04 01:34:52 +0800

    feat: modify Update sql and updateMovieHandler to add Optimistic Concurrency Control

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -168,6 +168,20 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.serverErrorResponse(w, r, err)
 		return
 	}
+
+	// Intercept any ErrEditConflict error and call the new editConflictResponse()
+	// helper.
+	err = app.models.Movies.Update(movie)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
 	// Write the updated movie record in a JSON response.
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
