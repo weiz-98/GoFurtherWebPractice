8dcdb0291a249c1bf0a28874c100ece6d43a2554
weiz-98 <qweqwe928928@gmail.com>
2024-06-03 22:34:57 +0800

    feat: add PUT updateMovieHandler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -16,6 +16,7 @@ func (app *application) routes() *httprouter.Router { // Initialize a new httpro
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
 
 	return router
 }
